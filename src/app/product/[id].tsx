6205712cdc56b6bbd5718ec7865a543dd9caa1c8
DanielCavalcantih <danielcavalcanti8000@gmail.com>
2024-02-08 12:33:18 -0300

    Finalizando projeto

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -6,15 +6,20 @@ import { Button } from "@/components/button";
 import { Feather } from "@expo/vector-icons"
 import { LinkButton } from "@/components/link-button";
 import { useCartStore } from "@/stores/cart-store";
+import { Redirect } from "expo-router";
 
 export default function Product() {
   const { id } = useLocalSearchParams();
   const cartStore = useCartStore();
   const navigation = useNavigation();
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
+
+  if (!product) {
+    return <Redirect href="/" />
+  }
 
   function handleAddToCart() {
-    cartStore.add(product);
+    cartStore.add(product!);
     navigation.goBack();
   }
 
@@ -25,7 +30,8 @@ export default function Product() {
         className="w-full h-52"
         resizeMode="cover"
       />
-      <View className="p-5 mt-8 flex-1">
+      <View className="p-5 mt-4 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
